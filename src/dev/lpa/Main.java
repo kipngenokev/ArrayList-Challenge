15e923e9018ace6a3f7800989908c7c3d802ccb4
brian070069 <kkipngeno409@gmail.com>
2024-08-15 23:57:03 +0300

    second commit

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -1,5 +1,7 @@
 package dev.lpa;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class Main {
@@ -8,10 +10,39 @@ public class Main {
     public static void main(String[] args) {
 
         boolean flag =true;
+        ArrayList<String> groceries = new ArrayList<>();
         while(flag){
             printActions();
             switch(Integer.parseInt(scanner.nextLine())){
+                case 1-> addItems(groceries);
+                case 2-> removeItems(groceries);
                 default->flag=false;
+            }
+            groceries.sort(Comparator.naturalOrder());
+            System.out.println(groceries);
+        }
+    }
+
+    private static void addItems(ArrayList<String> groceries) {
+        System.out.println("Add item(s) [separate items by coma]:");
+        String[] items =scanner.nextLine().split(",");
+        //groceries.addAll(List.of(items));
+        for(String i:items){
+            String trimmed =i.trim();
+            if(groceries.indexOf(trimmed)<0) {
+                groceries.add(trimmed);
+            }
+        }
+    }
+
+    private static void removeItems(ArrayList<String> groceries) {
+        System.out.println("Remove item(s) [separate items by coma]:");
+        String[] items =scanner.nextLine().split(",");
+
+        for(String i:items){
+            String trimmed =i.trim();
+            if(groceries.indexOf(trimmed)<0) {
+                groceries.remove(trimmed);
             }
         }
     }

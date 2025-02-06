a9950dc15ac5da0a8ce45edda119294a730a1c56
brian070069 <kkipngeno409@gmail.com>
2025-02-06 13:40:13 +0300

    final commit

---
+++ src/dev/Main.java
@@ -0,0 +1,65 @@
+package dev;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class Main {
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        boolean flag = true;
+        ArrayList<String> groceries = new ArrayList<>();
+        while(flag) {
+            printActions();
+            switch(Integer.parseInt(scanner.nextLine())){
+                case 1 -> addItems(groceries);
+                case 2 -> removeItems(groceries);
+                default -> flag = false;
+            }
+            groceries.sort(Comparator.naturalOrder());
+            System.out.println(groceries);
+        }
+
+    }
+
+    public static void addItems(ArrayList<String> groceries){
+        System.out.println("Enter item(s) to be added [Separate items by a comma:]");
+        String[] items = scanner.nextLine().split(",");
+        //groceries.addAll(List.of(items));
+        for(String i :items) {
+            String trimmed = i.trim();
+            if(groceries.indexOf(trimmed) < 0){
+                groceries.add(trimmed);
+            }
+        }
+    }
+
+    public static void removeItems(ArrayList<String> groceries){
+        System.out.println("Enter item(s) to be removed [Separate items by comma]");
+        String[] items = scanner.nextLine().split(",");
+
+        for (String i :items) {
+            if(groceries.contains(i)) {
+                groceries.remove(i);
+            } else {
+                System.out.println("Item not contained in the list!!!");
+            }
+        }
+    }
+
+    public static void printActions(){
+        String textBlock = """
+                Available actions:
+                                
+                0 - to shutdown
+                                
+                1 - to add item(s) to list (comma delimited list)
+                                
+                2 - to remove any items (comma delimited list)
+                                
+                Enter a number for which action you want to do:""";
+        System.out.print(textBlock + " ");
+    }
+
+}

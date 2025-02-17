cfb69b66a839d6b47b5cd5e07ddf2326d3cb11dc
brian070069 <kkipngeno409@gmail.com>
2025-02-17 14:29:09 +0300

    refined commit

--- src/dev/Main.java
+++ src/dev/Main.java
@@ -4,51 +4,25 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.Scanner;
 
-public class Main {
+public class Main{
     private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        boolean flag = true;
-        ArrayList<String> groceries = new ArrayList<>();
-        while(flag) {
-            printActions();
-            switch(Integer.parseInt(scanner.nextLine())){
-                case 1 -> addItems(groceries);
-                case 2 -> removeItems(groceries);
-                default -> flag = false;
-            }
-            groceries.sort(Comparator.naturalOrder());
-            System.out.println(groceries);
-        }
+         boolean flag = true;
+        ArrayList<String> grocery = new ArrayList<>();
+         while(flag) {
+             printActions();
+             switch(Integer.parseInt(scanner.nextLine())){
+                 case 1 -> addItems(grocery);
+                 case 2 -> removeItems(grocery);
+                 default -> flag = false;
+             }
+             grocery.sort(Comparator.naturalOrder());
+             System.out.println(grocery);
+         }
 
     }
-
-    public static void addItems(ArrayList<String> groceries){
-        System.out.println("Enter item(s) to be added [Separate items by a comma:]");
-        String[] items = scanner.nextLine().split(",");
-        //groceries.addAll(List.of(items));
-        for(String i :items) {
-            String trimmed = i.trim();
-            if(groceries.indexOf(trimmed) < 0){
-                groceries.add(trimmed);
-            }
-        }
-    }
-
-    public static void removeItems(ArrayList<String> groceries){
-        System.out.println("Enter item(s) to be removed [Separate items by comma]");
-        String[] items = scanner.nextLine().split(",");
-
-        for (String i :items) {
-            if(groceries.contains(i)) {
-                groceries.remove(i);
-            } else {
-                System.out.println("Item not contained in the list!!!");
-            }
-        }
-    }
-
-    public static void printActions(){
+    private static void printActions() {
         String textBlock = """
                 Available actions:
                                 
@@ -59,7 +33,28 @@ public class Main {
                 2 - to remove any items (comma delimited list)
                                 
                 Enter a number for which action you want to do:""";
+
         System.out.print(textBlock + " ");
     }
 
+    private static void addItems(ArrayList<String> grocery) {
+        System.out.println("Add item(s) [Separate items by comma]:");
+        String[] items = scanner.nextLine().split(",");
+//        grocery.addAll(List.of(items));
+         for (String i : items) {
+             String trimmed = i.trim();
+             if(grocery.indexOf(trimmed) < 0) {
+                 grocery.add(trimmed);
+             }
+         }
+    }
+
+    private static void removeItems(ArrayList<String> grocery) {
+        System.out.println("Enter item(s) for removal[separate items by a comma]:");
+        String[] items = scanner.nextLine().split(",");
+        for (String i : items) {
+            String trimmed = i.trim();
+            grocery.remove(trimmed);
+        }
+    }
 }

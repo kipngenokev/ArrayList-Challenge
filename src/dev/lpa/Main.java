a541213ad0335516455d033e0b3a2e3555a344e9
brian070069 <kkipngeno409@gmail.com>
2025-08-11 16:11:37 +0300

    successfully cracked the arrayList Challenge

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -4,12 +4,12 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.Scanner;
 
-public class Main {
+public class Main{
+
     private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-
-        boolean flag =  true;
+        boolean flag = true;
         ArrayList<String> groceries = new ArrayList<>();
         while(flag) {
             printActions();
@@ -21,11 +21,11 @@ public class Main {
             groceries.sort(Comparator.naturalOrder());
             System.out.println(groceries);
         }
-
     }
 
-    public static void printActions(){
-        String textBlock = """
+
+    public static void printActions() {
+        String textBlock =("""
                 Available actions:
                 
                 0 - to shutdown
@@ -34,31 +34,28 @@ public class Main {
                 
                 2 - to remove any items (comma delimited list)
                 
-                Enter a number for which action you want to do:
-                """;
-        System.out.print(textBlock + "  ");
+                Enter a number for which action you want to do:""");
+
+        System.out.print(textBlock + " ");
     }
 
-    public static void addItems(ArrayList<String> groceries) {
-        System.out.println("Add Item(s) [Separate items by comma");
-        String[] items = scanner.nextLine().split(",");
-        //        groceries.addAll(List.of(items));
-        for(String item: items) {
-            String trimmed = item.trim();
-            if(!groceries.contains(trimmed)) {
+    private static void addItems(ArrayList<String> groceries) {
+        System.out.println("Enter comma delimited item(s) to add:");
+        String[] itemsToAdd = scanner.nextLine().split(",");
+        for (String i : itemsToAdd) {
+            String trimmed = i.trim();
+            if (groceries.indexOf(trimmed) < 0) {
                 groceries.add(trimmed);
             }
         }
-
     }
 
-    public static void removeItems(ArrayList<String> groceries) {
-        System.out.println("Remove item(s) [Separate items by comma");
-        String[] items = scanner.nextLine().split(",");
-        for(String item : items) {
-            String trimmed =item.trim();
+    private static void removeItems(ArrayList<String> groceries) {
+        System.out.println("Enter a comma delimited list of item(s) to be removed:");
+        String[] itemsToRemove = scanner.nextLine().split(",");
+        for (String i : itemsToRemove) {
+            String trimmed = i.trim();
             groceries.remove(trimmed);
-
         }
     }
 
